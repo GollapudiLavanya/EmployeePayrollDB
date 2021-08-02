be00e1bc9a4639a1b3c3b42138856c7ec96f1f97
GollapuydiLavanya <lavanyagollapudi5166@gmail.com>
2021-08-02 07:22:18 -0700

    [LAVANYA]Insering Data

--- PayrollService.sql
+++ PayrollService.sql
@@ -8,4 +8,7 @@ id int identity(1,1) primary key,
 name varchar(200) not null,
 salary float,
 startDate date
-)
+);
+--------------UC3---------------
+insert into employee_payroll values('Anuja',24995,'2000-11-13'),('Meghana',88838,'2002-12-27')
+insert into employee_payroll (name,startDate,salary) values('Vindhya' ,'2003-03-22',34563)
