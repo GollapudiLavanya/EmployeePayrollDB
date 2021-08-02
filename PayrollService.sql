4452bee0e872eeab000e336a0cd245d7b47b83d9
GollapuydiLavanya <lavanyagollapudi5166@gmail.com>
2021-08-02 07:52:42 -0700

    [LAVANYA]Extend Table

--- PayrollService.sql
+++ PayrollService.sql
@@ -29,3 +29,13 @@ select gender,AVG(salary) as averageSalary from employee_payroll group by gender
 select gender,MIN(salary) as totalSalary from employee_payroll group by gender;
 select gender,MAX(salary) as totalSalary from employee_payroll group by gender;
 select gender,COUNT(salary) as countOfPeople from employee_payroll group by gender;
+--------------UC8---------------
+alter table employee_payroll add phonenumber bigint;
+alter table employee_payroll add address varchar(150) default 'bangalore';
+alter table employee_payroll add department varchar(150) not null default 'HR';
+
+update employee_payroll set phonenumber=8989343478,address='Chennai',department='Sales and Marketing' where name='Vindhya';
+update employee_payroll set phonenumber=7833829848,address='Chennai',department='HR' where name='Sai';
+update employee_payroll set phonenumber=8948373829,address='Bangalore',department='HR' where name='Venkat';
+update employee_payroll set phonenumber=6748399848,address='Bangalore',department='Sales and Marketing' where name='Meghana';
+update employee_payroll set phonenumber=9494943537,address='Mumbai',department='HR' where name='Anuja';
